7a8e3a6e2dedd9a19e0aecc6d5a14eeefbd057f1
magnuvau <Magnus.Austrheim@nav.no>
2019-05-02 15:35:41 +0200

    Spring Initializr

---
+++ src/main/java/no/nav/tjenestepensjon/simulering/TjenestepensjonSimuleringApplication.java
@@ -0,0 +1,13 @@
+package no.nav.tjenestepensjon.simulering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjenestepensjonSimuleringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjenestepensjonSimuleringApplication.class, args);
+	}
+
+}
