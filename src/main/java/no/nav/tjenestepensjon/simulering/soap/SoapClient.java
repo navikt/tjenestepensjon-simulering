4ec2e4fcf5d0e06fb89426456b692012147e415d
magnuvau <Magnus.Austrheim@nav.no>
2019-05-15 09:08:05 +0200

    Added hentStillingsprosentListe to SoapClient

--- src/main/java/no/nav/tjenestepensjon/simulering/soap/SoapClient.java
+++ src/main/java/no/nav/tjenestepensjon/simulering/soap/SoapClient.java
@@ -1,19 +1,30 @@
 package no.nav.tjenestepensjon.simulering.soap;
 
-import no.nav.ekstern.pensjon.tjenester.tjenestepensjonsimulering.meldinger.v1.Stillingsprosent;
+import no.nav.ekstern.pensjon.tjenester.tjenestepensjonsimulering.meldinger.v1.HentStillingsprosentListeResponse;
+import no.nav.ekstern.pensjon.tjenester.tjenestepensjonsimulering.meldinger.v1.ObjectFactory;
+import no.nav.tjenestepensjon.simulering.domain.Stillingsprosent;
+import no.nav.tjenestepensjon.simulering.mapper.StillingsprosentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
-public class SoapClient {
+public class SoapClient extends WebServiceGatewaySupport {
 
     @Autowired
     private WebServiceTemplate webServiceTemplate;
 
     public List<Stillingsprosent> hentStillingsprosentListe() {
-        return null;
+        var request = new ObjectFactory().createHentStillingsprosentListeRequest();
+        var response = (HentStillingsprosentListeResponse) webServiceTemplate.marshalSendAndReceive(request);
+        var mapper = new StillingsprosentMapper();
+
+        return response.getStillingsprosentListe().stream()
+            .map(mapper::mapToStillingsprosent)
+            .collect(Collectors.toList());
     }
 }
---
+++ src/test/java/no/nav/tjenestepensjon/simulering/soap/SoapClientTests.java
@@ -0,0 +1,17 @@
+package no.nav.tjenestepensjon.simulering.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SoapClientTests {
+
+    @Autowired
+    private SoapClient soapClient;
+
+    @Test
+    public void context() {
+
+    }
+}
