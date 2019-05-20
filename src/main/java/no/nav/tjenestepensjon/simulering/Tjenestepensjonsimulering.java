23064f19024a76d72a9b36c826332555752e0bf5
magnuvau <Magnus.Austrheim@nav.no>
2019-05-20 08:06:50 +0200

    Added simulerPensjon() to SoapClient

--- src/main/java/no/nav/tjenestepensjon/simulering/Tjenestepensjonsimulering.java
+++ src/main/java/no/nav/tjenestepensjon/simulering/Tjenestepensjonsimulering.java
@@ -1,10 +1,13 @@
 package no.nav.tjenestepensjon.simulering;
 
 import no.nav.tjenestepensjon.simulering.domain.Stillingsprosent;
+import no.nav.tjenestepensjon.simulering.rest.OutgoingResponse;
 
 import java.util.List;
 
 public interface Tjenestepensjonsimulering {
 
     List<Stillingsprosent> getStillingsprosenter();
+
+    List<OutgoingResponse.SimulertPensjon> simulerPensjon();
 }
--- src/main/java/no/nav/tjenestepensjon/simulering/soap/SoapClient.java
+++ src/main/java/no/nav/tjenestepensjon/simulering/soap/SoapClient.java
@@ -2,13 +2,16 @@ package no.nav.tjenestepensjon.simulering.soap;
 
 import no.nav.ekstern.pensjon.tjenester.tjenestepensjonsimulering.meldinger.v1.HentStillingsprosentListeResponse;
 import no.nav.ekstern.pensjon.tjenester.tjenestepensjonsimulering.meldinger.v1.ObjectFactory;
+import no.nav.ekstern.pensjon.tjenester.tjenestepensjonsimulering.meldinger.v1.SimulerOffentligTjenestepensjonResponse;
 import no.nav.tjenestepensjon.simulering.Tjenestepensjonsimulering;
 import no.nav.tjenestepensjon.simulering.domain.Stillingsprosent;
 import no.nav.tjenestepensjon.simulering.mapper.StillingsprosentMapper;
+import no.nav.tjenestepensjon.simulering.rest.OutgoingResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,5 +33,29 @@ public class SoapClient extends WebServiceGatewaySupport implements Tjenestepens
         return response.getStillingsprosentListe().stream()
                 .map(mapper::mapToStillingsprosent)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<OutgoingResponse.SimulertPensjon> simulerPensjon() {
+        var request = new ObjectFactory().createSimulerOffentligTjenestepensjonRequest();
+        var response = (SimulerOffentligTjenestepensjonResponse) webServiceTemplate.marshalSendAndReceive(request);
+        var simuletPensjonListe = new ArrayList<OutgoingResponse.SimulertPensjon>();
+
+        for (var simulertPensjon : response.getSimulertPensjonListe()) {
+            var mappedSimulertPensjon = new OutgoingResponse.SimulertPensjon();
+            mappedSimulertPensjon.setTpnr(simulertPensjon.getTpnr());
+            mappedSimulertPensjon.setNavnOrdning(simulertPensjon.getNavnOrdning());
+            mappedSimulertPensjon.setInkluderteOrdninger(simulertPensjon.getInkludertOrdningListe());
+            mappedSimulertPensjon.setLeverandorUrl(simulertPensjon.getLeverandorUrl());
+//            mappedSimulertPensjon.setInkluderteTpnr();
+//            mappedSimulertPensjon.setUtelatteTpnr();
+//            mappedSimulertPensjon.setStatus();
+//            mappedSimulertPensjon.setFeilkode();
+//            mappedSimulertPensjon.setFeilbeskrivelse();
+//            mappedSimulertPensjon.setUtbetalingsperioder();
+            simuletPensjonListe.add(mappedSimulertPensjon);
+        }
+
+        return simuletPensjonListe;
     }
 }
