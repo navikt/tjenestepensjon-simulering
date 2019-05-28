0b83774655051ded1bbf594b3735ab571ad10ff3
Jacob Meidell <jacob.meidell@nav.no>
2019-05-28 09:51:39 +0200

    Changed the fault-handler such that it wont gobble up any unknown errors. If the detail is not present, or any exceptions occur during unmarshalling/casting, the faultcode and faultstring from the fault-node will be used.

--- src/main/java/no/nav/tjenestepensjon/simulering/soap/SoapFaultHandler.java
+++ src/main/java/no/nav/tjenestepensjon/simulering/soap/SoapFaultHandler.java
@@ -8,6 +8,7 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Component;
 import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.client.core.FaultMessageResolver;
+import org.springframework.ws.soap.SoapFault;
 import org.springframework.ws.soap.SoapFaultDetailElement;
 import org.springframework.ws.soap.SoapMessage;
 
@@ -25,20 +26,27 @@ public class SoapFaultHandler implements FaultMessageResolver {
 
     @Override
     public void resolveFault(WebServiceMessage message) {
+        SoapFaultException exception;
         SoapMessage soapMessage = (SoapMessage) message;
-        SoapFaultDetailElement soapFaultDetail = soapMessage.getSoapBody().getFault().getFaultDetail().getDetailEntries().next();
-        JAXBElement jaxbElement = (JAXBElement) jaxb2Marshaller.unmarshal(soapFaultDetail.getSource());
-        StelvioFault knownFault = (StelvioFault) jaxbElement.getValue();
-        SoapFaultException resolved = new SoapFaultException(knownFault.getClass(), knownFault.getErrorMessage());
-        LOG.warn("Resolved fault from SOAP-invocation: {}", resolved.toString());
-        throw resolved;
+        SoapFault soapFault = soapMessage.getSoapBody().getFault();
+        try {
+            SoapFaultDetailElement soapFaultDetail = soapFault.getFaultDetail().getDetailEntries().next();
+            JAXBElement jaxbElement = (JAXBElement) jaxb2Marshaller.unmarshal(soapFaultDetail.getSource());
+            StelvioFault knownFault = (StelvioFault) jaxbElement.getValue();
+            exception = new SoapFaultException(knownFault.getClass().getName(), knownFault.getErrorMessage());
+            LOG.warn("Resolved known fault from SoapFaultDetail: {}", exception.toString());
+        } catch (Exception ex) {
+            exception = new SoapFaultException(soapFault.getFaultCode().toString(), soapFault.getFaultStringOrReason());
+            LOG.warn("Could not resolve known error from SoapFaultDetail. Resolved from SaopFault: {}", exception.toString());
+        }
+        throw exception;
     }
 
     static class SoapFaultException extends RuntimeException {
-        private Class exception;
+        private String exception;
         private String message;
 
-        public SoapFaultException(Class original, String originalMessage) {
+        public SoapFaultException(String original, String originalMessage) {
             this.exception = original;
             this.message = originalMessage;
         }
