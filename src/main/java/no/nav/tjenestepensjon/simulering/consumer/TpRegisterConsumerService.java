fb12e5aa1fb37727e86d895875839e71605a9b4d
magnuvau <Magnus.Austrheim@nav.no>
2019-06-05 09:07:24 +0200

    Added authorization header for tpregisteret requests

--- src/main/java/no/nav/tjenestepensjon/simulering/consumer/TpRegisterConsumerService.java
+++ src/main/java/no/nav/tjenestepensjon/simulering/consumer/TpRegisterConsumerService.java
@@ -1,11 +1,12 @@
 package no.nav.tjenestepensjon.simulering.consumer;
 
+import java.util.Collections;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +19,9 @@ public class TpRegisterConsumerService implements TpRegisterConsumer {
     private String tpRegisterUrl;
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Autowired
+    private TokenClient tokenClient;
+
     @Value("${TP_REGISTERET_URL}")
     public void setTpRegisterUrl(String tpRegisterUrl) {
         this.tpRegisterUrl = tpRegisterUrl;
@@ -25,10 +29,13 @@ public class TpRegisterConsumerService implements TpRegisterConsumer {
 
     @Override
     public List<TPOrdning> getTpOrdningerForPerson(String fnr) throws NoTpOrdningerFoundException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + tokenClient.getOidcAccessToken());
         ResponseEntity<List<TPOrdning>> responseEntity = restTemplate
                 .exchange(tpRegisterUrl + "/person/" + fnr + "/tpordninger",
                         HttpMethod.GET,
-                        null,
+                        new HttpEntity<>("parameters", headers),
                         new ParameterizedTypeReference<List<TPOrdning>>() {
                         });
         if (responseEntity.getBody().size() == 0) {
