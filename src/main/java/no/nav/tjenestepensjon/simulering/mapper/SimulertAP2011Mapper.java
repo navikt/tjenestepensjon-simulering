9d8838fc0a2baeda9755b5d7bbdbed5554bbfefb
Jacob Meidell <jacob.meidell@nav.no>
2019-06-06 08:52:28 +0200

    Refactored the mapping of SimulertAP2011 to handle the scenario of gradert and fullt uttak separately. Also; setting the missing value for "simulertHeltUttakEtter67Ar" if fullt forstegangsuttak is after the user has turned 67.

--- src/main/java/no/nav/tjenestepensjon/simulering/mapper/SimulertAP2011Mapper.java
+++ src/main/java/no/nav/tjenestepensjon/simulering/mapper/SimulertAP2011Mapper.java
@@ -1,33 +1,62 @@
 package no.nav.tjenestepensjon.simulering.mapper;
 
 import static no.nav.tjenestepensjon.simulering.mapper.SimuleringsdataMapper.mapToSimuleringsdata;
+import static no.nav.tjenestepensjon.simulering.util.Utils.createDayResolutionCalendar;
+import static no.nav.tjenestepensjon.simulering.util.Utils.getBirthDate;
+import static no.nav.tjenestepensjon.simulering.util.Utils.isSameDay;
 
-import java.util.List;
+import java.util.Calendar;
+import java.util.Date;
 
 import no.nav.ekstern.pensjon.tjenester.tjenestepensjonsimulering.meldinger.v1.SimulertAP2011;
 import no.nav.tjenestepensjon.simulering.rest.IncomingRequest.Simuleringsperiode;
 
 public class SimulertAP2011Mapper {
 
-    public static SimulertAP2011 mapToSimulertAP2011(List<Simuleringsperiode> simuleringsperiodeList) {
+    /**
+     * Mapping function for scenario fullt uttak (forsteUttak == 100%).
+     * If the user is older than 67 at the time of fullt uttak, the values from the forsteUttak is also set on {@link SimulertAP2011#getSimulertHeltUttakEtter67Ar()}.
+     * Prior to the transition to the new kap.20 information-model, PEN would provide this value to the ESB, but this information is "lost" in the new model.
+     *
+     * @param forsteUttak {@link Simuleringsperiode} for forsteuttak
+     * @param fnr fodselsnr
+     * @return {@link SimulertAP2011}
+     * @see "TPEN630 SimulerTP and FPEN032 hentBeregningsinformasjonForTP in PEN for further reference"
+     */
+    public static SimulertAP2011 mapFulltUttak(Simuleringsperiode forsteUttak, String fnr) {
         SimulertAP2011 simulertAP2011 = new SimulertAP2011();
-        if (simuleringsperiodeList.size() == 1) {
-            simulertAP2011.setSimulertForsteuttak(mapToSimuleringsdata(simuleringsperiodeList.get(0)));
-        } else {
-            //Will have two periods if gradert førsteuttak
-            simulertAP2011.setSimulertForsteuttak(mapToSimuleringsdata(simuleringsperiodeList.stream().min(SimulertAP2011Mapper::order).get()));
-            simulertAP2011.setSimulertHeltUttakEtter67Ar(mapToSimuleringsdata(simuleringsperiodeList.stream().max(SimulertAP2011Mapper::order).get()));
+        simulertAP2011.setSimulertForsteuttak(mapToSimuleringsdata(forsteUttak));
+        if (isUttakEtter67Ar(forsteUttak, fnr)) {
+            simulertAP2011.setSimulertHeltUttakEtter67Ar(mapToSimuleringsdata(forsteUttak));
         }
         return simulertAP2011;
     }
 
-    private static int order(Simuleringsperiode o1, Simuleringsperiode o2) {
-        if (o1.getDatoFom().before(o2.getDatoFom())) {
-            return -1;
-        } else if (o1.getDatoFom().after(o2.getDatoFom())) {
-            return 1;
-        } else {
-            return 0;
-        }
+    /**
+     * Mapping function for scenario gradert uttak (forstUttak < 100%)
+     * Unlike the scenario for fullt uttak, PEN will provide two periods for gradert uttak. In this case, the value of {@link SimulertAP2011#getSimulertHeltUttakEtter67Ar()}
+     * should be set without checking the age (this is handled by PEN).
+     *
+     * @param forsteUttak {@link Simuleringsperiode} for forsteuttak
+     * @param heltUttak {@link Simuleringsperiode} for helt uttak
+     * @return {@link SimulertAP2011}
+     * @see "TPEN630 SimulerTP and FPEN032 hentBeregningsinformasjonForTP in PEN for further reference"
+     */
+    public static SimulertAP2011 mapGradertUttak(Simuleringsperiode forsteUttak, Simuleringsperiode heltUttak) {
+        SimulertAP2011 simulertAP2011 = new SimulertAP2011();
+        simulertAP2011.setSimulertForsteuttak(mapToSimuleringsdata(forsteUttak));
+        simulertAP2011.setSimulertHeltUttakEtter67Ar(mapToSimuleringsdata(heltUttak));
+        return simulertAP2011;
+    }
+
+    private static boolean isUttakEtter67Ar(Simuleringsperiode simuleringsperiode, String fnr) {
+        Date dateAt67Years = findDateAt67Years(fnr);
+        return isSameDay(simuleringsperiode.getDatoFom(), dateAt67Years) || simuleringsperiode.getDatoFom().after(dateAt67Years);
+    }
+
+    private static Date findDateAt67Years(String fnr) {
+        Calendar calendar = createDayResolutionCalendar(getBirthDate(fnr));
+        calendar.add(Calendar.YEAR, 67);
+        return calendar.getTime();
     }
 }
--- src/test/java/no/nav/tjenestepensjon/simulering/mapper/SimulertAP2011MapperTest.java
+++ src/test/java/no/nav/tjenestepensjon/simulering/mapper/SimulertAP2011MapperTest.java
@@ -2,18 +2,18 @@ package no.nav.tjenestepensjon.simulering.mapper;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
-import static no.nav.tjenestepensjon.simulering.mapper.SimulertAP2011Mapper.mapToSimulertAP2011;
+import static no.nav.tjenestepensjon.simulering.mapper.SimulertAP2011Mapper.mapFulltUttak;
+import static no.nav.tjenestepensjon.simulering.mapper.SimulertAP2011Mapper.mapGradertUttak;
 import static no.nav.tjenestepensjon.simulering.rest.IncomingRequest.Simuleringsperiode;
+import static no.nav.tjenestepensjon.simulering.util.Utils.createDate;
 
-import java.time.Instant;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import no.nav.ekstern.pensjon.tjenester.tjenestepensjonsimulering.meldinger.v1.SimulertAP2011;
@@ -21,35 +21,35 @@ import no.nav.tjenestepensjon.simulering.rest.IncomingRequest.Delytelse;
 
 class SimulertAP2011MapperTest {
 
-    private static Simuleringsperiode periode1;
-    private static Simuleringsperiode periode2;
+    private static Simuleringsperiode forsteUttak;
+    private static Simuleringsperiode heltUttak;
+    //Turns 67 in 2027
+    private static final String fnr = "01016092500";
 
-    @BeforeAll
-    static void beforeAll() {
-        periode1 = new Simuleringsperiode();
-        periode1.setDatoFom(Date.from(Instant.parse("2007-12-03T10:15:30.00Z")));
-        periode1.setPoengArTom1991(15);
-        periode1.setPoengArFom1992(17);
-        periode1.setSluttpoengtall(5.5d);
-        periode1.setAnvendtTrygdetid(64);
-        periode1.setForholdstall(100.0);
-        periode1.setUforegradVedOmregning(10);
-        periode1.setDelytelser(List.of(
+    @BeforeEach
+    void beforeAll() {
+        forsteUttak = new Simuleringsperiode();
+        forsteUttak.setPoengArTom1991(15);
+        forsteUttak.setPoengArFom1992(17);
+        forsteUttak.setSluttpoengtall(5.5d);
+        forsteUttak.setAnvendtTrygdetid(64);
+        forsteUttak.setForholdstall(100.0);
+        forsteUttak.setUforegradVedOmregning(10);
+        forsteUttak.setDelytelser(List.of(
                 new Delytelse("basisgp", 25000d),
                 new Delytelse("basispt", 5000d),
                 new Delytelse("basistp", 15000d),
                 new Delytelse("skjermingstillegg", 7000d)
         ));
 
-        periode2 = new Simuleringsperiode();
-        periode2.setDatoFom(Date.from(Instant.parse("2015-12-03T10:15:30.00Z")));
-        periode2.setPoengArTom1991(5);
-        periode2.setPoengArFom1992(7);
-        periode2.setSluttpoengtall(2.5d);
-        periode2.setAnvendtTrygdetid(31);
-        periode2.setForholdstall(2.5);
-        periode2.setUforegradVedOmregning(35);
-        periode2.setDelytelser(List.of(
+        heltUttak = new Simuleringsperiode();
+        heltUttak.setPoengArTom1991(5);
+        heltUttak.setPoengArFom1992(9);
+        heltUttak.setSluttpoengtall(2.5d);
+        heltUttak.setAnvendtTrygdetid(31);
+        heltUttak.setForholdstall(2.5);
+        heltUttak.setUforegradVedOmregning(35);
+        heltUttak.setDelytelser(List.of(
                 new Delytelse("basisgp", 5000d),
                 new Delytelse("basispt", 500d),
                 new Delytelse("basistp", 5000d),
@@ -58,38 +58,10 @@ class SimulertAP2011MapperTest {
     }
 
     @Test
-    void sortsFomDateAscending() {
-        Simuleringsperiode earliest = new Simuleringsperiode();
-        earliest.setDatoFom(Date.from(Instant.parse("2005-12-03T10:15:30.00Z")));
-        earliest.setPoengArTom1991(1);
-        earliest.setPoengArFom1992(1);
-        earliest.setSluttpoengtall(1.0);
-        earliest.setAnvendtTrygdetid(1);
-        earliest.setForholdstall(1.0);
-        earliest.setDelytelser(List.of());
-
-        Simuleringsperiode latest = new Simuleringsperiode();
-        latest.setDatoFom(Date.from(Instant.parse("2067-12-03T10:15:30.00Z")));
-        latest.setPoengArTom1991(2);
-        latest.setPoengArFom1992(2);
-        latest.setSluttpoengtall(2.0);
-        latest.setAnvendtTrygdetid(2);
-        latest.setForholdstall(2.0);
-        latest.setDelytelser(List.of());
-
-        SimulertAP2011 simulertAP2011 = mapToSimulertAP2011(List.of(periode2, earliest, latest, periode1));
-
-        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), is(1));
-        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), not(periode1.getPoengArFom1992()));
-        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), not(periode2.getPoengArFom1992()));
-        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getPoengArFom1992(), is(2));
-        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getPoengArFom1992(), not(periode1.getPoengArFom1992()));
-        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getPoengArFom1992(), not(periode2.getPoengArFom1992()));
-    }
-
-    @Test
     void shouldMapWhenFulltForstegangsuttak() {
-        SimulertAP2011 simulertAP2011 = mapToSimulertAP2011(List.of(periode1));
+        forsteUttak.setDatoFom(createDate(2026, Calendar.JANUARY, 1));
+
+        SimulertAP2011 simulertAP2011 = mapFulltUttak(forsteUttak, fnr);
 
         assertThat(simulertAP2011.getSimulertForsteuttak(), is(notNullValue()));
         assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar(), is(nullValue()));
@@ -97,7 +69,10 @@ class SimulertAP2011MapperTest {
 
     @Test
     void shouldMapWhenGradertForstegangsuttak() {
-        SimulertAP2011 simulertAP2011 = mapToSimulertAP2011(List.of(periode1, periode2));
+        forsteUttak.setDatoFom(createDate(2027, Calendar.DECEMBER, 3));
+        heltUttak.setDatoFom(createDate(2030, Calendar.DECEMBER, 3));
+
+        SimulertAP2011 simulertAP2011 = mapGradertUttak(forsteUttak, heltUttak);
 
         assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArTom1991(), is(15));
         assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), is(17));
@@ -111,7 +86,7 @@ class SimulertAP2011MapperTest {
         assertThat(simulertAP2011.getSimulertForsteuttak().getUforegradVedOmregning(), is(10));
 
         assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getPoengArTom1991(), is(5));
-        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getPoengArFom1992(), is(7));
+        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getPoengArFom1992(), is(9));
         assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getSluttpoengtall(), is(2.5d));
         assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getAnvendtTrygdetid(), is(31));
         assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getBasisgp(), is(5000d));
@@ -120,5 +95,66 @@ class SimulertAP2011MapperTest {
         assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getForholdstallUttak(), is(2.5d));
         assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getSkjermingstillegg(), is(700d));
         assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getUforegradVedOmregning(), is(35));
+    }
+
+    @Test
+    void gradertAndHeltUttakBefore67() {
+        forsteUttak.setDatoFom(createDate(2022, Calendar.JANUARY, 1));
+        heltUttak.setDatoFom(createDate(2024, Calendar.JUNE, 6));
+
+        SimulertAP2011 simulertAP2011 = mapFulltUttak(forsteUttak, fnr);
+
+        assertThat(simulertAP2011.getSimulertForsteuttak(), is(notNullValue()));
+        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), is(17));
+        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar(), is(nullValue()));
+    }
+
+    @Test
+    void gradertBefore67AndHeltUttakAfter67() {
+        forsteUttak.setDatoFom(createDate(2024, Calendar.APRIL, 5));
+        heltUttak.setDatoFom(createDate(2028, Calendar.OCTOBER, 11));
+
+        SimulertAP2011 simulertAP2011 = mapGradertUttak(forsteUttak, heltUttak);
+
+        assertThat(simulertAP2011.getSimulertForsteuttak(), is(notNullValue()));
+        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), is(17));
+        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar(), is(notNullValue()));
+        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getPoengArFom1992(), is(9));
+    }
+
+    @Test
+    void gradertAfter67AndHeltUttakAfter67() {
+        forsteUttak.setDatoFom(createDate(2029, Calendar.FEBRUARY, 1));
+        heltUttak.setDatoFom(createDate(2030, Calendar.MARCH, 8));
+
+        SimulertAP2011 simulertAP2011 = mapGradertUttak(forsteUttak, heltUttak);
+
+        assertThat(simulertAP2011.getSimulertForsteuttak(), is(notNullValue()));
+        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), is(17));
+        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar(), is(notNullValue()));
+        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar().getPoengArFom1992(), is(9));
+    }
+
+    @Test
+    void heltUttakBefore67() {
+        forsteUttak.setDatoFom(createDate(2025, Calendar.FEBRUARY, 1));
+
+        SimulertAP2011 simulertAP2011 = mapFulltUttak(forsteUttak, fnr);
+
+        assertThat(simulertAP2011.getSimulertForsteuttak(), is(notNullValue()));
+        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), is(17));
+        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar(), is(nullValue()));
+    }
+
+    @Test
+    void heltUttakAfter67() {
+        forsteUttak.setDatoFom(createDate(2029, Calendar.FEBRUARY, 1));
+
+        SimulertAP2011 simulertAP2011 = mapFulltUttak(forsteUttak, fnr);
+
+        assertThat(simulertAP2011.getSimulertForsteuttak(), is(notNullValue()));
+        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), is(17));
+        assertThat(simulertAP2011.getSimulertHeltUttakEtter67Ar(), is(notNullValue()));
+        assertThat(simulertAP2011.getSimulertForsteuttak().getPoengArFom1992(), is(17));
     }
 }
