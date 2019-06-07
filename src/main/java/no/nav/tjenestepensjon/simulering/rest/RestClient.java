b9dcd26bc63e949d80d2f5462954a97555f8e796
K153294 <martin.knutsen@nav.no>
2019-06-07 14:35:40 +0200

    Added dummy WebClient implementation.

--- src/main/java/no/nav/tjenestepensjon/simulering/rest/RestClient.java
+++ src/main/java/no/nav/tjenestepensjon/simulering/rest/RestClient.java
@@ -1,26 +1,50 @@
 package no.nav.tjenestepensjon.simulering.rest;
 
 import no.nav.tjenestepensjon.simulering.Tjenestepensjonsimulering;
+import no.nav.tjenestepensjon.simulering.consumer.TokenClient;
 import no.nav.tjenestepensjon.simulering.domain.Stillingsprosent;
 import no.nav.tjenestepensjon.simulering.domain.TPOrdning;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
+import org.springframework.web.reactive.function.client.WebClient;
 import java.util.List;
 
 @Component
 public class RestClient implements Tjenestepensjonsimulering {
+    private WebClient webClient = WebClient.create();
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestClient.class);
+
+    private final TokenClient tokenClient;
+
+    public RestClient(TokenClient tokenClient) {
+        this.tokenClient = tokenClient;
+    }
+
     @Override
     public List<Stillingsprosent> getStillingsprosenter(String fnr, TPOrdning tpOrdning) {
-        List<Stillingsprosent> stillingsprosenter = new ArrayList<>();
-
+        List<Stillingsprosent> stillingsprosenter = webClient.get()
+                .uri("http://localhost")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .header("Authorization", "Bearer " + tokenClient.getOidcAccessToken())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Stillingsprosent>>() {})
+                .block();
         return stillingsprosenter;
     }
 
     @Override
     public List<OutgoingResponse.SimulertPensjon> simulerPensjon(IncomingRequest incomingRequest, List<TPOrdning> tpOrdningList, TPOrdning latest) {
-        var simuletPensjonListe = new ArrayList<OutgoingResponse.SimulertPensjon>();
-
+        List<OutgoingResponse.SimulertPensjon> simuletPensjonListe = webClient.get()
+                .uri("http://localhost")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .header("Authorization", "Bearer " + tokenClient.getOidcAccessToken())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<OutgoingResponse.SimulertPensjon>>() {})
+                .block();
         return simuletPensjonListe;
     }
 }
