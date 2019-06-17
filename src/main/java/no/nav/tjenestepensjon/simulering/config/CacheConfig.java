2855ac9f7a4c33c48eb9a836d0264ebf05133996
Jacob Meidell <jacob.meidell@nav.no>
2019-06-17 08:17:11 +0200

    Made a couple of changes to leverage the framework cache-auto-config.

--- src/main/java/no/nav/tjenestepensjon/simulering/config/CacheConfig.java
+++ src/main/java/no/nav/tjenestepensjon/simulering/config/CacheConfig.java
@@ -3,15 +3,11 @@ package no.nav.tjenestepensjon.simulering.config;
 import static java.util.concurrent.TimeUnit.DAYS;
 import static java.util.concurrent.TimeUnit.HOURS;
 
-import java.util.List;
-
 import com.github.benmanes.caffeine.cache.Caffeine;
 
 import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCache;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,13 +17,6 @@ public class CacheConfig {
 
     public static final String TP_ORDNING_LEVERANDOR_CACHE = "TP_ORDNING_LEVERANDOR_CACHE";
     public static final String TP_ORDNING_PERSON_CACHE = "TP_ORDNING_PERSON_CACHE";
-
-    @Bean
-    public CacheManager cacheManager() {
-        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
-        simpleCacheManager.setCaches(List.of(tpOrdningLeverandorCache(), tpOrdningPersonCache()));
-        return simpleCacheManager;
-    }
 
     @Bean
     public Cache tpOrdningLeverandorCache() {
