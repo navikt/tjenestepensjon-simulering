e5fb9b1a2f586e942b48fb2419b2274ca9a28fd7
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2020-02-05 09:15:33 +0100

    PL-883: Fix JWK implementation.

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/config/TokenProviderConfig.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/config/TokenProviderConfig.kt
@@ -1,10 +1,8 @@
 package no.nav.tjenestepensjon.simulering.config
 
-import io.micrometer.core.instrument.util.StringUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.*
 
 @Configuration
 class TokenProviderConfig {
--- src/main/kotlin/no/nav/tjenestepensjon/simulering/config/TokenStoreConfig.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/config/TokenStoreConfig.kt
@@ -1,19 +1,20 @@
 package no.nav.tjenestepensjon.simulering.config
 
 import no.nav.tjenestepensjon.simulering.config.TokenProviderConfig.TokenProvider
-import no.nav.tjenestepensjon.simulering.config.jwk.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.provider.token.TokenStore
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
 import org.springframework.security.oauth2.provider.token.store.JwtClaimsSetVerifier
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore
 
 @Configuration
 class TokenStoreConfig(private val tokenProviders: List<TokenProvider>, private val claimsSetVerifier: JwtClaimsSetVerifier) {
     @Bean
-    fun tokenStore(): TokenStore = JwkTokenStore(tokenProviders, jwtAccessTokenConverter(), claimsSetVerifier)
+    fun tokenStore(): TokenStore = JwkTokenStore(tokenProviders.map(TokenProvider::jwksUrl), jwtAccessTokenConverter(), claimsSetVerifier)
 
     @Bean
-    fun jwtAccessTokenConverter() =
-            JwkVerifyingJwtAccessTokenConverter(JwkDefinitionSource(tokenProviders))
-
+    fun jwtAccessTokenConverter() = JwtAccessTokenConverter().apply {
+        jwtClaimsSetVerifier = claimsSetVerifier
+    }
 }
