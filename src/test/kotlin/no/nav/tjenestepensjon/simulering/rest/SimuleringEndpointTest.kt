4b7bb4d9a9e91558a4e5d0a3a3c67443d825ad97
Magnus Austrheim <Magnus.Austrheim@nav.no>
2020-03-12 09:47:07 +0100

    Fix simulering endpoint test

--- src/test/kotlin/no/nav/tjenestepensjon/simulering/rest/SimuleringEndpointTest.kt
+++ src/test/kotlin/no/nav/tjenestepensjon/simulering/rest/SimuleringEndpointTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -23,6 +24,7 @@ class SimuleringEndpointTest {
     fun secureEndpointOkWithValidToken() {
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/simulering")
+                .header(AUTHORIZATION, "Bearer ${TokenProviderStub.accessToken}")
         ).andExpect(MockMvcResultMatchers.status().isBadRequest)
     }
 
