88d38757c58bbfc6a0966d7e3fe473206c6f1b21
TheMather1 <the.mather1@gmail.com>
2022-01-24 10:51:45 +0100

    Use correct authority url.

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/service/AADClient.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/service/AADClient.kt
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Service
 class AADClient(
     @Value("\${AZURE_APP_CLIENT_ID}") private val clientId: String,
     @Value("\${AZURE_APP_CLIENT_SECRET}") private val clientSecret: String,
-    @Value("\${AZURE_OPENID_CONFIG_ISSUER}") private val issuer: String
+    @Value("\${AZURE_APP_WELL_KNOWN_URL}") private val authority: String
 ) {
     private val app =
-        ConfidentialClientApplication.builder(clientId, createFromSecret(clientSecret)).authority(issuer).build()
+        ConfidentialClientApplication.builder(clientId, createFromSecret(clientSecret)).authority(authority).build()
 
     fun getToken(vararg scope: String): String =
         app.acquireToken(ClientCredentialParameters.builder(scope.toSet()).build()).get().accessToken()
