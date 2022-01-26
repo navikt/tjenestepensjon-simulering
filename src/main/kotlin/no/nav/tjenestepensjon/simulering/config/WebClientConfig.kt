8961394d104ff1ddd4f663a4a35d87160105fc61
TheMather1 <the.mather1@gmail.com>
2022-01-26 13:57:55 +0100

    Add proxy for AAD.

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/config/WebClientConfig.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/config/WebClientConfig.kt
@@ -9,19 +9,27 @@ import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
 import reactor.netty.tcp.TcpClient
 import reactor.netty.transport.ProxyProvider.Proxy.HTTP
+import java.net.InetSocketAddress
+import java.net.Proxy
 
 @Configuration
 class WebClientConfig {
 
+    private val proxyAddress = InetSocketAddress("webproxy.nais.nav.no", 8080)
+
     private final val tcpClient: TcpClient = TcpClient.create()
             .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECT_TIMEOUT_MILLIS)
             .doOnConnected { it.addHandlerLast(ReadTimeoutHandler(READ_TIMEOUT_MILLIS / 1000)) }
-            .proxy { it.type(HTTP).host("webproxy-nais.nav.no").port(8088).nonProxyHosts(".*(tpregisteret|tpconfig|security-token-service|isso).*") }
+            .proxy {
+                it.type(HTTP).address(proxyAddress).nonProxyHosts(".*(tp-api|security-token-service|isso).*") }
 
     @get:Bean
     val webClient: WebClient = WebClient.builder()
             .clientConnector(ReactorClientHttpConnector(HttpClient.from(tcpClient)))
             .build()
+
+    @Bean
+    fun proxy() = Proxy(Proxy.Type.HTTP, proxyAddress)
 
     companion object{
         private const val CONNECT_TIMEOUT_MILLIS = 3000
--- src/main/kotlin/no/nav/tjenestepensjon/simulering/service/AADClient.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/service/AADClient.kt
@@ -5,15 +5,17 @@ import com.microsoft.aad.msal4j.ClientCredentialParameters
 import com.microsoft.aad.msal4j.ConfidentialClientApplication
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.net.Proxy
 
 @Service
 class AADClient(
-    @Value("\${AZURE_APP_CLIENT_ID}") private val clientId: String,
-    @Value("\${AZURE_APP_CLIENT_SECRET}") private val clientSecret: String,
-    @Value("\${AZURE_APP_WELL_KNOWN_URL}") private val authority: String
+    @Value("\${AZURE_APP_CLIENT_ID}") clientId: String,
+    @Value("\${AZURE_APP_CLIENT_SECRET}") clientSecret: String,
+    @Value("\${AZURE_APP_WELL_KNOWN_URL}") authority: String,
+    proxy: Proxy = Proxy.NO_PROXY
 ) {
     private val app =
-        ConfidentialClientApplication.builder(clientId, createFromSecret(clientSecret)).authority(authority).build()
+        ConfidentialClientApplication.builder(clientId, createFromSecret(clientSecret)).authority(authority).proxy(proxy).build()
 
     fun getToken(vararg scope: String): String =
         app.acquireToken(ClientCredentialParameters.builder(scope.toSet()).build()).get().accessToken()
