979d2c8204910e944c9b1d7f79ad297658834705
TheMather1 <the.mather1@gmail.com>
2022-05-31 10:28:12 +0200

    Improve objectMapper configuration

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/config/ObjectMapperConfig.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/config/ObjectMapperConfig.kt
@@ -3,10 +3,10 @@ package no.nav.tjenestepensjon.simulering.config
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.fasterxml.jackson.module.kotlin.jsonMapper
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.LocalDate
@@ -15,14 +15,13 @@ import java.time.LocalDate
 class ObjectMapperConfig {
 
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper()
-        .registerKotlinModule()
-        .registerModule(
-            JavaTimeModule()
-                .addSerializer(LocalDate::class.java, LocalDateEpochSerializer())
-        ).apply {
-            enable(ACCEPT_CASE_INSENSITIVE_ENUMS)
-        }
+    fun objectMapper() = jsonMapper {
+        enable(ACCEPT_CASE_INSENSITIVE_ENUMS)
+        addModules(
+            kotlinModule(),
+            JavaTimeModule().addSerializer(LocalDate::class.java, LocalDateEpochSerializer())
+        )
+    }
 
 
     class LocalDateEpochSerializer : JsonSerializer<LocalDate>() {
