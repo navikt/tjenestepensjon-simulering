4d391071e0a7c88eb8194305bfbbb8efeef65644
paalfb <paalfb@yahoo.se>
2023-04-25 15:53:11 +0200

    handling return values from TpClient findForhold.

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/service/TpClient.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/service/TpClient.kt
@@ -3,15 +3,19 @@ package no.nav.tjenestepensjon.simulering.service
 import no.nav.tjenestepensjon.simulering.config.CacheConfig.Companion.TP_ORDNING_LEVERANDOR_CACHE
 import no.nav.tjenestepensjon.simulering.config.CacheConfig.Companion.TP_ORDNING_PERSON_CACHE
 import no.nav.tjenestepensjon.simulering.config.CacheConfig.Companion.TP_ORDNING_TSSID_CACHE
-import no.nav.tjenestepensjon.simulering.model.domain.TPOrdning
 import no.nav.tjenestepensjon.simulering.exceptions.NoTpOrdningerFoundException
 import no.nav.tjenestepensjon.simulering.model.domain.FNR
+import no.nav.tjenestepensjon.simulering.model.domain.TPOrdning
 import no.nav.tjenestepensjon.simulering.model.domain.Tjenestepensjon
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+import org.springframework.web.server.ResponseStatusException
+import reactor.core.publisher.Mono
 
 @Service
 class TpClient(
@@ -20,16 +24,34 @@ class TpClient(
     @Value("\${TP_URL}") private var tpUrl: String,
     @Value("\${TP_SCOPE}") private val tpScope: String
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
 
     fun getTpOrdningerForPerson(fnr: FNR) = findForhold(fnr).map {
         TPOrdning(tpId = it.ordning, tssId = findTssId(it.ordning))
     }
 
     @Cacheable(TP_ORDNING_PERSON_CACHE)
-    fun findForhold(fnr: FNR) = webClient.get().uri("$tpUrl/api/tjenestepensjon").headers {
-        it["fnr"] = fnr.fnr
-        it.setBearerAuth(aadClient.getToken(tpScope))
-    }.retrieve().bodyToMono<Tjenestepensjon>().block()?.forhold?.takeUnless(List<Tjenestepensjon.Forhold>::isEmpty)
+    fun findForhold(fnr: FNR) = webClient.get()
+        .uri("$tpUrl/api/tjenestepensjon")
+        .headers {
+            it["fnr"] = fnr.fnr
+            it.setBearerAuth(aadClient.getToken(tpScope))
+        }.exchangeToMono {
+            when (it.statusCode().value()) {
+                200 -> it.bodyToMono<Tjenestepensjon>().flatMap { tp ->
+                    if(tp.forhold.isEmpty()) Mono.error(NoTpOrdningerFoundException("No Tp-ordning found for person."))
+                    else Mono.just(tp.forhold)
+                }
+                404 -> Mono.error(NoTpOrdningerFoundException("No Tp-ordning found for person."))
+                else -> it.bodyToMono<String>().defaultIfEmpty("<NULL>").flatMap { body ->
+                    Mono.error(badGateway("Status code ${it.statusCode()} with message: $body}"))
+                }
+            }
+        }.onErrorMap {
+            if (it !is ResponseStatusException) badGateway(it.message) else it
+        }.doOnSuccess {
+            log.info("Successfully fetched data.")
+        }.block()
         ?: throw NoTpOrdningerFoundException("No Tp-ordning found for person.")
 
     @Cacheable(TP_ORDNING_LEVERANDOR_CACHE)
@@ -41,4 +63,8 @@ class TpClient(
     fun findTssId(tpId: String): String =
         webClient.get().uri("$tpUrl/api/tpconfig/tssnr/$tpId").retrieve().bodyToMono<String>().block()!!
 
+    fun badGateway(logMessage: String?): ResponseStatusException {
+        log.error("Error fetching data from TP: $logMessage")
+        return ResponseStatusException(HttpStatus.BAD_GATEWAY)
+    }
 }
