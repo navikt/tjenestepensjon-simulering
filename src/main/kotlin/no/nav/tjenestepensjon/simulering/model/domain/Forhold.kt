474b85e579957e049030359dc45fd6525a22409b
Pål Fleicher Børjeson <paalfb@yahoo.se>
2023-04-27 15:30:41 +0200

    JsonIgnoreProperites på Forhold

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/model/domain/Forhold.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/model/domain/Forhold.kt
@@ -1,5 +1,8 @@
 package no.nav.tjenestepensjon.simulering.model.domain
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Forhold(
     var ordning: String
 )
