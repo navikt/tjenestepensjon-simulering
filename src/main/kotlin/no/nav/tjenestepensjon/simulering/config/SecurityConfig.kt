bb6bf6e95e9b67e23e7f043d6f146a414a630af4
Pål Fleicher Børjeson <paalfb@yahoo.se>
2023-10-13 14:36:13 +0200

    Fix. Bruke AntPathRequestMatcher
    Co-authored-by: Jens Chr. Madsen <Jens.Christian.Madsen@nav.no>
    Co-authored-by: Mathias Sand Jahren <the.mather1@gmail.com>
    Co-authored-by: Martin Knutsen <martin.knutsen@nav.no>

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/config/SecurityConfig.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/config/SecurityConfig.kt
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher
 
 @Configuration
 @EnableWebSecurity
@@ -16,11 +18,11 @@ class SecurityConfig(
     private val issuerResolver = JwtIssuerAuthenticationManagerResolver(issuers.split(','))
 
     @Bean
-    fun configure(http: HttpSecurity) = http.run {
-        csrf().disable()
-        authorizeHttpRequests()
-            .requestMatchers("/actuator/**").permitAll()
-            .anyRequest().authenticated()
+    fun configure(http: HttpSecurity): DefaultSecurityFilterChain = http.run {
+        csrf { it.disable() }
+        authorizeHttpRequests {
+            it.requestMatchers(antMatcher("/actuator/**")).permitAll().anyRequest().authenticated()
+        }
         oauth2ResourceServer {
             it.authenticationManagerResolver(issuerResolver)
         }
