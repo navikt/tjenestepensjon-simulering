a399436c42b4ae86f945e0d0673efe8861caf71c
Anton Fofanov <anton.fofanov@nav.no>
2024-01-06 20:41:22 +0100

    update tests

--- src/test/kotlin/no/nav/tjenestepensjon/simulering/v1/rest/SimuleringEndpointSecurityTest.kt
+++ src/test/kotlin/no/nav/tjenestepensjon/simulering/v1/rest/SimuleringEndpointSecurityTest.kt
@@ -21,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
@@ -28,6 +29,7 @@ import org.springframework.test.web.servlet.post
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestInstance(PER_CLASS)
+@TestPropertySource(locations = ["classpath:application.yml"])
 class SimuleringEndpointSecurityTest {
 
     @Autowired
