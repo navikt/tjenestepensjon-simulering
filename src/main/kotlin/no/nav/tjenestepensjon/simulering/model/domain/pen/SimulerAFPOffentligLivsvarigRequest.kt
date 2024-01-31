a2689459eead4590e58b4df9cefd79a872048cfc
Anton Fofanov <anton.fofanov@nav.no>
2024-01-31 15:48:19 +0100

    remove unused variables from request to simulerAfpOffentligLivsvarig endpoint

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/model/domain/pen/SimulerAFPOffentligLivsvarigRequest.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/model/domain/pen/SimulerAFPOffentligLivsvarigRequest.kt
@@ -1,9 +1,5 @@
 package no.nav.tjenestepensjon.simulering.model.domain.pen
 
-import no.nav.tjenestepensjon.simulering.v2.models.domain.SivilstandCodeEnum
 import java.time.LocalDate
 
-data class SimulerAFPOffentligLivsvarigRequest(val fnr: String, val fodselsdato: LocalDate, val fremtidigeInntekter: List<FremtidigInntekt>,
-                                               val sivilstandVedPensjonering: SivilstandCodeEnum, val fom: LocalDate,
-                                               val arIUtlandetEtter16: Int, val epsPensjon: Boolean, val eps2G: Boolean
-    )
+data class SimulerAFPOffentligLivsvarigRequest(val fnr: String, val fodselsdato: LocalDate, val fremtidigeInntekter: List<FremtidigInntekt>, val fom: LocalDate)
