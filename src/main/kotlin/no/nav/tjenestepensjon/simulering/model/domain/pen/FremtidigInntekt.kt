fa01c5b41161d1537be78793dfea0b55e4f936fc
Anton Fofanov <anton.fofanov@nav.no>
2024-02-12 14:23:59 +0100

    add json ignore annotation

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/model/domain/pen/FremtidigInntekt.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/model/domain/pen/FremtidigInntekt.kt
@@ -1,6 +1,8 @@
 package no.nav.tjenestepensjon.simulering.model.domain.pen
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class FremtidigInntekt(val belop: Int, @JsonProperty("fraOgMed") val fom: LocalDate)
