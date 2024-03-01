660e7ade67b505be102ac5aeb48498205f0fb4ec
Anton Fofanov <anton.fofanov@nav.no>
2024-03-01 12:32:08 +0100

    log steps of calculattion

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/v3/afp/AFPOffentligLivsvarigSimuleringService.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/v3/afp/AFPOffentligLivsvarigSimuleringService.kt
@@ -9,19 +9,25 @@ import no.nav.tjenestepensjon.simulering.model.domain.popp.InntektPeriode
 import no.nav.tjenestepensjon.simulering.model.domain.popp.SimulerAFPBeholdningGrunnlagRequest
 import no.nav.tjenestepensjon.simulering.service.AFPBeholdningClient
 import no.nav.tjenestepensjon.simulering.service.PenClient
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class AFPOffentligLivsvarigSimuleringService(val afpBeholdningClient: AFPBeholdningClient, val penClient: PenClient) {
+    private val log = LoggerFactory.getLogger(javaClass)
 
     fun simuler(request: SimulerAFPOffentligLivsvarigRequest): List<AfpOffentligLivsvarigYtelseMedDelingstall> {
         val aldreForDelingstall: List<AlderForDelingstall> = AlderForDelingstallBeregner.bestemAldreForDelingstall(request.fodselsdato, request.fom)
+        log.info("Aldere for delingstall: $aldreForDelingstall")
 
         val requestToAFPBeholdninger = SimulerAFPBeholdningGrunnlagRequest(request.fnr, request.fom, request.fremtidigeInntekter.map { InntektPeriode(it.fom, it.belop) })
         val beholdningerMedAldreForDelingstall: List<PensjonsbeholdningMedDelingstallAlder> = afpBeholdningClient.simulerAFPBeholdningGrunnlag(requestToAFPBeholdninger)
-            .map { periode -> PensjonsbeholdningMedDelingstallAlder(periode.pensjonsBeholdning, aldreForDelingstall.first { it.datoVedAlder.year == periode.fom.year }) }
-
+            .map { periode ->
+                log.info("Simulert beholdning for periode: $periode")
+                PensjonsbeholdningMedDelingstallAlder(periode.pensjonsBeholdning, aldreForDelingstall.first { it.datoVedAlder.year == periode.fom.year }) }
+        log.info("Beholdninger med alder for delingstall: $beholdningerMedAldreForDelingstall")
         val delingstallListe = penClient.hentDelingstall(request.fodselsdato.year, beholdningerMedAldreForDelingstall.map { it.alderForDelingstall.alder }.toList())
+        log.info("Delingstall: $delingstallListe")
 
         val beregningsgrunnlag = beholdningerMedAldreForDelingstall
             .map {
