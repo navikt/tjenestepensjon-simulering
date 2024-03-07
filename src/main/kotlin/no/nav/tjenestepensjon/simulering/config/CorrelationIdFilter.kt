a9d5cab43704dc2139f0fd6b7ce0be7a9c72a9b8
Anton Fofanov <anton.fofanov@nav.no>
2024-03-07 15:47:02 +0100

    apply correlation id and consumer id. add access logg

---
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/config/CorrelationIdFilter.kt
@@ -0,0 +1,33 @@
+package no.nav.tjenestepensjon.simulering.config
+
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.MDC
+import org.springframework.stereotype.Component
+
+@Component
+class CorrelationIdFilter : Filter {
+    companion object {
+        const val CORRELATION_ID_HTTP_HEADER = "Nav-Call-Id"
+        const val CONSUMER_ID_HTTP_HEADER = "Nav-Consumer-Id"
+        const val CORRELATION_ID = "transaction"
+        const val CONSUMER_ID = "user"
+    }
+
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        val httpRequest = request as HttpServletRequest
+        val correlationId = httpRequest.getHeader(CORRELATION_ID_HTTP_HEADER) ?: java.util.UUID.randomUUID().toString()
+        val consumerId = httpRequest.getHeader(CONSUMER_ID_HTTP_HEADER) ?: "unknown_consumer"
+        MDC.put(CORRELATION_ID, correlationId)
+        MDC.put(CONSUMER_ID, consumerId)
+        try {
+            chain.doFilter(request, response)
+        } finally {
+            MDC.remove(CORRELATION_ID)
+            MDC.remove(CONSUMER_ID)
+        }
+    }
+}
---
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/config/TomcatAccessLogCustomizer.kt
@@ -0,0 +1,16 @@
+package no.nav.tjenestepensjon.simulering.config
+
+import ch.qos.logback.access.tomcat.LogbackValve
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
+import org.springframework.stereotype.Component
+
+@Component
+class TomcatAccessLogCustomizer : WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    override fun customize(factory: TomcatServletWebServerFactory) {
+        val logbackValve = LogbackValve()
+        logbackValve.name = "Logback Access"
+        logbackValve.filename = "logback-access.xml"
+        factory.addEngineValves(logbackValve)
+    }
+}
