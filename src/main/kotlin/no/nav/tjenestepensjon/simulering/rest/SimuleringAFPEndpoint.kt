520b184dcf5543c19c87a6be71040ad22f34320f
Anton Fofanov <anton.fofanov@nav.no>
2024-08-08 12:25:52 +0200

    ikke hent afp offentlig leverandørnavn. La konsumentkoden stå for senere bruk

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/rest/SimuleringAFPEndpoint.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/rest/SimuleringAFPEndpoint.kt
@@ -1,29 +1,22 @@
 package no.nav.tjenestepensjon.simulering.rest
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import no.nav.tjenestepensjon.simulering.model.domain.AktivTpOrdningDto
-import no.nav.tjenestepensjon.simulering.model.domain.FNR
 import no.nav.tjenestepensjon.simulering.model.domain.pen.SimulerAFPOffentligLivsvarigRequest
 import no.nav.tjenestepensjon.simulering.model.domain.pen.SimulerAFPOffentligLivsvarigResponse
-import no.nav.tjenestepensjon.simulering.service.TpClient
 import no.nav.tjenestepensjon.simulering.v3.afp.AFPOffentligLivsvarigSimuleringService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SimuleringAFPEndpoint(val afpOffentligLivsvarigSimuleringService: AFPOffentligLivsvarigSimuleringService, val tpClient: TpClient) {
+class SimuleringAFPEndpoint(val afpOffentligLivsvarigSimuleringService: AFPOffentligLivsvarigSimuleringService) {
     private val log = KotlinLogging.logger {}
 
     @PostMapping("/simulering/afp-offentlig-livsvarig")
     fun simulerAfpOffentligLivsvarig(@RequestBody request: SimulerAFPOffentligLivsvarigRequest): SimulerAFPOffentligLivsvarigResponse {
-
         log.debug { "Simulerer AFP Offentlig Livsvarig for request: $request" }
         validateRequest(request)
-
-        val tpLeverandoererNavn = tpClient.findAktiveForhold(FNR(request.fnr)).joinToString(separator = ", ") { it.navn }
-        log.debug { "Beregner AFP Offentlig for en bruker som er medlem i tp-ordning(er): <$tpLeverandoererNavn>" }
-        return SimulerAFPOffentligLivsvarigResponse(request.fnr, afpOffentligLivsvarigSimuleringService.simuler(request), tpLeverandoererNavn)
+        return SimulerAFPOffentligLivsvarigResponse(request.fnr, afpOffentligLivsvarigSimuleringService.simuler(request))
     }
 
     private fun validateRequest(request: SimulerAFPOffentligLivsvarigRequest) {
