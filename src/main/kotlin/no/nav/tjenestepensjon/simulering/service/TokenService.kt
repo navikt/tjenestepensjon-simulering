f001bdb9550ac7d4f9a97e1b34fae51cb12ccee8
Anton Fofanov <anton.fofanov@nav.no>
2024-08-27 17:40:09 +0200

    PEK-519 hent OIDC-token for simulering. Liten refactoring

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/service/TokenService.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/service/TokenService.kt
@@ -3,6 +3,6 @@ package no.nav.tjenestepensjon.simulering.service
 import no.nav.tjenestepensjon.simulering.domain.Token
 
 interface TokenService {
-    val oidcAccessToken: Token?
+    val oidcAccessToken: Token
     val samlAccessToken: Token
 }
