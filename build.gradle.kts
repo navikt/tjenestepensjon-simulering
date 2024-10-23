a38f30c5ff10e2c0f9ef67c1dd0ddb35d53da3b8
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-10-23 10:19:41 +0200

    Bump jvm from 1.9.20 to 1.9.23

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ group = "no.nav.tjenestepensjon"
 description = "tjenestepensjon-simulering"
 
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.6"
