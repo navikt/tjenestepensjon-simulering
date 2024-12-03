68cabb0f786d1a8a0e63b6b45007bf840983baae
Anton Fofanov <anton.fofanov@nav.no>
2024-12-03 13:21:06 +0100

    Unngå bad request fra spk i ping

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/v2/rest/RestClient.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/v2/rest/RestClient.kt
@@ -1,18 +1,14 @@
 package no.nav.tjenestepensjon.simulering.v2.rest
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import no.nav.tjenestepensjon.simulering.model.domain.Pensjonsbeholdningsperiode
 import no.nav.tjenestepensjon.simulering.model.domain.TPOrdningIdDto
-import no.nav.tjenestepensjon.simulering.v2.models.domain.SivilstandCodeEnum
-import no.nav.tjenestepensjon.simulering.v2.models.request.*
+import no.nav.tjenestepensjon.simulering.v2.models.request.SimulerPensjonRequestV2
 import no.nav.tjenestepensjon.simulering.v2.models.response.SimulerOffentligTjenestepensjonResponse
-import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.exception.TjenestepensjonSimuleringException
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientRequestException
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.bodyToMono
-import java.time.LocalDate
 
 @Service
 class RestClient(private val spkPre2025Client: WebClient) {
@@ -39,72 +35,91 @@ class RestClient(private val spkPre2025Client: WebClient) {
                 .retrieve()
                 .bodyToMono(String::class.java)
                 .block() ?: "No body received"
-        } catch (e: WebClientResponseException){
+        } catch (e: WebClientResponseException) {
             val msg = "Failed to ping SPK at url $url, got: ${e.responseBodyAsString} for request $request, ${e.message}"
             log.error(e) { msg }
             msg
-        } catch (e: WebClientRequestException){
+        } catch (e: WebClientRequestException) {
             val msg = "Failed to ping SPK at uri ${e.uri} with request $request"
             log.error(e) { msg }
             msg
         }
-
     }
 
-    private fun dummyRequest(fnr: String = "14866297763"): DummyRequestV2 {
-        val now = LocalDate.now()
-        return DummyRequestV2(
-            fnr = Foedselsnummer(fnr),
-            fodselsdato = "14-06-1962",
-            sisteTpnr = "3010",
-            sivilstandkode = SivilstandCodeEnum.UGIF,
-            inntektListe = listOf(
-                Inntekt(
-                    datoFom = LocalDate.of(now.year - 2, 1, 1),
-                    inntekt = 900000.0
-                )
-            ),
-            simuleringsperiodeListe = listOf(
-                Simuleringsperiode(
-                    datoFom = LocalDate.of(now.year + 1, 1, 1),
-                    folketrygdUttaksgrad = 20,
-                    stillingsprosentOffentlig = 100,
-                    simulerAFPOffentligEtterfulgtAvAlder = false
-                )
-            ),
-            simuleringsdataListe = listOf(
-                Simuleringsdata(
-                    datoFom = LocalDate.of(now.year + 1, 1, 1),
-                    andvendtTrygdetid = 40,
-                    poengArTom1991 = 30,
-                    poengArFom1992 = 40,
-                    uforegradVedOmregning = null,
-                    basisgp = 96883.0,
-                    basispt = 97829.58,
-                    basistp = 0.0,
-                    delingstallUttak = 16.81,
-                    forholdstallUttak = 1.184,
-                    sluttpoengtall = 5.28
-                )
-            )
-        )
-    }
-
-
-    data class DummyRequestV2(
-        var fnr: Foedselsnummer,
-        var fodselsdato: String,
-        var sisteTpnr: String,
-        var sprak: String? = null,
-        var simulertAFPOffentlig: SimulertAFPOffentlig? = null,
-        var simulertAFPPrivat: SimulertAFPPrivat? = null,
-        var sivilstandkode: SivilstandCodeEnum,
-        var inntektListe: List<Inntekt>,
-        var pensjonsbeholdningsperiodeListe: List<Pensjonsbeholdningsperiode> = emptyList(),
-        var simuleringsperiodeListe: List<Simuleringsperiode>,
-        var simuleringsdataListe: List<Simuleringsdata>,
-        var tpForholdListe: List<TpForhold> = emptyList()
-    )
-
-    data class Foedselsnummer(val fnr: String)
+    private fun dummyRequest() : String =
+        """ 
+            {
+            "sisteTpnr": "3010",
+            "fnr": "14866297763",
+            "fodselsdato": "1962-06-14",
+            "sivilstandkode": "GIFT",
+            "sprak": "nb",
+            "simuleringsperioder": [{
+            "datoFom": "2025-06-01",
+            "folketrygdUttaksgrad": 100,
+            "stillingsprosentOffentlig": 0,
+            "simulerAFPOffentligEtterfulgtAvAlder": "J"
+            }],
+            "simuleringsdata": [{
+            "datoFom": "2025-06-01",
+            "poengArTom1991": 11,
+            "poengArFom1992": 29,
+            "sluttpoengtall": 5.56,
+            "anvendtTrygdetid": 40,
+            "forholdstall": 1.099,
+            "delingstall": 15.73,
+            "uforegradVedOmregning": 0,
+            "basisgp": 89872.0,
+            "basistp": 237768.0,
+            "basispt": 0.0
+            }],
+            "simulertAFPOffentlig": {
+            "simulertAFPOffentligBrutto": 348036,
+            "tpi": 785180.0
+            },
+            "simulertAFPPrivat": null,
+            "tpForhold": [{
+            "tpnr": "3010",
+            "opptjeningsperioder": [{
+            "stillingsprosent": 76,
+            "datoFom": "1987-08-01",
+            "datoTom": "1988-07-31",
+            "faktiskHovedlonn": 785180,
+            "stillingsuavhengigTilleggslonn": 0,
+            "aldersgrense": 70,
+            "risikofellesskap": null
+            },
+            {
+            "stillingsprosent": 100,
+            "datoFom": "1988-08-01",
+            "datoTom": "2009-05-02",
+            "faktiskHovedlonn": 785180,
+            "stillingsuavhengigTilleggslonn": 0,
+            "aldersgrense": 70,
+            "risikofellesskap": null
+            },
+            {
+            "stillingsprosent": 100,
+            "datoFom": "2009-05-03",
+            "datoTom": "9999-12-31",
+            "faktiskHovedlonn": 785180,
+            "stillingsuavhengigTilleggslonn": 0,
+            "aldersgrense": 70,
+            "risikofellesskap": null
+            }
+            ]
+            }],
+            "pensjonsbeholdningsperioder": [{
+            "datoFom": "2025-06-01",
+            "pensjonsbeholdning": 4409390,
+            "garantipensjonsbeholdning": -883500,
+            "garantitilleggsbeholdning": -1022307
+            }],
+            "inntekter": [{
+            "datoFom": "2010-10-01",
+            "inntekt": 0.0
+            }],
+            "tpnr": "3010"
+            }
+        """.trimIndent()
 }
