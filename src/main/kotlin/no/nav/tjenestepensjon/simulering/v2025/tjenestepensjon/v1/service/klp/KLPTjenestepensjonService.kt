1ee528af5d9214f9969f46823eaa5d10cb148c27
martinstorvoll <martin.storvoll@nav.no>
2025-01-14 14:52:30 +0100

    Lagt til mock for KLP

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/klp/KLPTjenestepensjonService.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/klp/KLPTjenestepensjonService.kt
@@ -1,6 +1,9 @@
 package no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.service.klp
 
+import no.nav.tjenestepensjon.simulering.common.AlderUtil.bestemUttaksalderVedDato
 import no.nav.tjenestepensjon.simulering.ping.Pingable
+import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Maanedsutbetaling
+import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Ordning
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.SimulertTjenestepensjonMedMaanedsUtbetalinger
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.dto.request.SimulerTjenestepensjonRequestDto
 import org.springframework.stereotype.Service
@@ -9,8 +12,25 @@ import org.springframework.stereotype.Service
 class KLPTjenestepensjonService(private val client: KLPTjenestepensjonClient) : Pingable {
 
     fun simuler(request: SimulerTjenestepensjonRequestDto): Result<SimulertTjenestepensjonMedMaanedsUtbetalinger> {
-        return Result.failure(RuntimeException("Aggregation is not implemented for KLP"))
+
+        val maanedsutbetalingMock = Maanedsutbetaling(
+            fraOgMedDato = request.uttaksdato,
+            fraOgMedAlder = bestemUttaksalderVedDato(fodselsdato = request.foedselsdato, date = request.uttaksdato),
+            maanedsBeloep = 5000,
+        )
+        val klpResponseMock = SimulertTjenestepensjonMedMaanedsUtbetalinger(
+            tpLeverandoer = "Kommunal landspensjonskasse",
+            ordningsListe = arrayListOf(Ordning("3100")),
+            utbetalingsperioder = arrayListOf(maanedsutbetalingMock),
+            aarsakIngenUtbetaling = emptyList(),
+            betingetTjenestepensjonErInkludert = false,
+            serviceData = emptyList()
+        )
+
+        return Result.success(klpResponseMock)
     }
 
     override fun ping() = client.ping()
+
+
 }
