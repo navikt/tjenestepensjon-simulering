1b3e7e1c0bfec89058c0c45266182c582aa38143
martinstorvoll <martin.storvoll@nav.no>
2025-01-20 12:39:01 +0100

    Lagt til feature toggle for klp

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/klp/KLPTjenestepensjonService.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/klp/KLPTjenestepensjonService.kt
@@ -1,17 +1,27 @@
 package no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.service.klp
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.tjenestepensjon.simulering.common.AlderUtil.bestemUttaksalderVedDato
 import no.nav.tjenestepensjon.simulering.ping.Pingable
+import no.nav.tjenestepensjon.simulering.service.FeatureToggleService
+import no.nav.tjenestepensjon.simulering.service.FeatureToggleService.Companion.PEN_715_SIMULER_SPK
+import no.nav.tjenestepensjon.simulering.service.FeatureToggleService.Companion.SIMULER_KLP
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Maanedsutbetaling
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Ordning
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.SimulertTjenestepensjonMedMaanedsUtbetalinger
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.dto.request.SimulerTjenestepensjonRequestDto
+import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.exception.TjenestepensjonSimuleringException
 import org.springframework.stereotype.Service
 
 @Service
-class KLPTjenestepensjonService(private val client: KLPTjenestepensjonClient) : Pingable {
+class KLPTjenestepensjonService(private val client: KLPTjenestepensjonClient, private val featureToggleService: FeatureToggleService) : Pingable {
+    private val log = KotlinLogging.logger {}
 
     fun simuler(request: SimulerTjenestepensjonRequestDto): Result<SimulertTjenestepensjonMedMaanedsUtbetalinger> {
+
+        if (!featureToggleService.isEnabled(SIMULER_KLP)) {
+            return loggOgReturn()
+        }
 
         val maanedsutbetalingMock = Maanedsutbetaling(
             fraOgMedDato = request.uttaksdato,
@@ -25,7 +35,6 @@ class KLPTjenestepensjonService(private val client: KLPTjenestepensjonClient) : 
             maanedsBeloep = 6000,
         )
 
-
         val klpResponseMock = SimulertTjenestepensjonMedMaanedsUtbetalinger(
             tpLeverandoer = "Kommunal Landspensjonskasse",
             ordningsListe = arrayListOf(Ordning("3100")),
@@ -36,6 +45,12 @@ class KLPTjenestepensjonService(private val client: KLPTjenestepensjonClient) : 
         )
 
         return Result.success(klpResponseMock)
+    }
+
+    private fun loggOgReturn(): Result<SimulertTjenestepensjonMedMaanedsUtbetalinger> {
+        val message = "Simulering av tjenestepensjon hos KLP er slått av"
+        log.warn { message }
+        return Result.failure(TjenestepensjonSimuleringException(message))
     }
 
     override fun ping() = client.ping()
