26b51d75f035e3415086c79471da6ed9f866fc19
martinstorvoll <martin.storvoll@nav.no>
2025-02-04 10:51:43 +0100

    Tester for klp service

--- src/test/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/klp/KLPTjenestepensjonServiceTest.kt
+++ src/test/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/klp/KLPTjenestepensjonServiceTest.kt
@@ -8,13 +8,16 @@ import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Simuler
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Utbetalingsperiode
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.exception.TpOrdningStoettesIkkeException
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.service.TjenestepensjonV2025ServiceTest.Companion.dummyRequest
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.util.ReflectionTestUtils.setField
 import java.time.LocalDate
+import kotlin.test.assertEquals
 
 @SpringBootTest
 class KLPTjenestepensjonServiceTest {
@@ -25,22 +28,57 @@ class KLPTjenestepensjonServiceTest {
     @Autowired
     private lateinit var klpTjenestepensjonService: KLPTjenestepensjonService
 
+    @MockitoBean
+    private lateinit var client: KLPTjenestepensjonClient
 
-@Test
-fun `simulering skal ikke skje naar feature toggle er av`() {
-    val req = dummyRequest("1963-02-05")
-    `when`(featureToggleService.isEnabled(SIMULER_KLP)).thenReturn(false)
+    @BeforeEach
+    fun initialize() {
+        setField(klpTjenestepensjonService, "activeProfiles", "prod-gcp")
+    }
 
-    val res : Result<SimulertTjenestepensjonMedMaanedsUtbetalinger> = klpTjenestepensjonService.simuler(req,"4080")
+    @Test
+    fun `simulering skal ikke skje naar feature toggle er av`() {
+        val req = dummyRequest("1963-02-05")
+        `when`(featureToggleService.isEnabled(SIMULER_KLP)).thenReturn(false)
 
-    val exception = res.exceptionOrNull()
-    assertTrue(res.isFailure)
-    assertTrue(exception is TpOrdningStoettesIkkeException)
-}
+        val res : Result<SimulertTjenestepensjonMedMaanedsUtbetalinger> = klpTjenestepensjonService.simuler(req,"4080")
 
-    fun dummyResult(inkluderBTP: Boolean = false) : Result<SimulertTjenestepensjon> {
+        val exception = res.exceptionOrNull()
+        assertTrue(res.isFailure)
+        assertTrue(exception is TpOrdningStoettesIkkeException)
+    }
+
+    @Test
+    fun `BTP og OFTP skal ikke inkluderes i simulering`() {
+        val req = dummyRequest("1963-02-05")
+        `when`(featureToggleService.isEnabled(SIMULER_KLP)).thenReturn(true)
+        `when`(client.simuler(req, "4080")).thenReturn(dummyResult())
+
+        val res : Result<SimulertTjenestepensjonMedMaanedsUtbetalinger> = klpTjenestepensjonService.simuler(req,"4080")
+
+        assertTrue(res.isSuccess)
+        val tjenestepensjon = res.getOrNull()
+        assertFalse(tjenestepensjon!!.betingetTjenestepensjonErInkludert)
+        assertEquals(tjenestepensjon.utbetalingsperioder.size, 2)
+    }
+
+    @Test
+    fun `ikke simuler med BTP fra klp`() {
+        val req = dummyRequest("1963-02-05")
+        `when`(client.simuler(req,"3010")).thenReturn(dummyResult())
+        `when`(featureToggleService.isEnabled(SIMULER_KLP)).thenReturn(true)
+
+        val res : Result<SimulertTjenestepensjonMedMaanedsUtbetalinger> = klpTjenestepensjonService.simuler(req,"3010")
+
+        assertTrue(res.isSuccess)
+        val tjenestepensjon = res.getOrNull()
+        assertNotNull(tjenestepensjon)
+        assertFalse(tjenestepensjon!!.betingetTjenestepensjonErInkludert)
+    }
+
+    fun dummyResult() : Result<SimulertTjenestepensjon> {
         return Result.success(SimulertTjenestepensjon(
-            tpLeverandoer = "spk",
+            tpLeverandoer = "klp",
             ordningsListe = listOf(Ordning("3010")),
             utbetalingsperioder = listOf(
                 Utbetalingsperiode(
@@ -49,26 +87,21 @@ fun `simulering skal ikke skje naar feature toggle er av`() {
                     ytelseType = "SAERALDERSPAASLAG"
                 ),
                 Utbetalingsperiode(
-                    fom = LocalDate.parse("2026-03-01"),
+                    fom = LocalDate.parse("2027-03-01"),
                     maanedligBelop = 4000,
                     ytelseType = "OT6370"
                 ),
                 Utbetalingsperiode(
-                    fom = LocalDate.parse("2026-01-01"),
-                    maanedligBelop = 1000,
-                    ytelseType = "PAASLAG"
-                ),
-                Utbetalingsperiode(
-                    fom = LocalDate.parse("2026-01-01"),
-                    maanedligBelop = 2000,
-                    ytelseType = "APOF2020"
-                ),Utbetalingsperiode(
                     fom = LocalDate.parse("2026-10-01"),
                     maanedligBelop = 2000,
                     ytelseType = "BTP"
+                ),Utbetalingsperiode(
+                    fom = LocalDate.parse("2026-11-01"),
+                    maanedligBelop = 2000,
+                    ytelseType = "OAFP"
                 ),
             ),
-            betingetTjenestepensjonErInkludert = inkluderBTP
+            betingetTjenestepensjonErInkludert = true
         ))
     }
 }
