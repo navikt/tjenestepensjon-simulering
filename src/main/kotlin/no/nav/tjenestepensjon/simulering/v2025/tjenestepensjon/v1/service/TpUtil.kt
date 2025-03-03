afe5d208f02a830f5387fc7944c9a17aadcd251c
martinstorvoll <martin.storvoll@nav.no>
2025-03-03 10:23:05 +0100

    Lagt til log av forskjellig AFP

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/TpUtil.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/TpUtil.kt
@@ -1,11 +1,18 @@
 package no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.service
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.tjenestepensjon.simulering.common.AlderUtil.bestemUttaksalderVedDato
+import no.nav.tjenestepensjon.simulering.model.domain.pen.FremtidigInntekt
+import no.nav.tjenestepensjon.simulering.model.domain.pen.SimulerAFPOffentligLivsvarigRequest
+import no.nav.tjenestepensjon.simulering.v2025.afp.v1.AFPOffentligLivsvarigSimuleringService
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Maanedsutbetaling
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Utbetalingsperiode
+import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.dto.request.SimulerTjenestepensjonRequestDto
 import java.time.LocalDate
 
 object TpUtil {
+    private lateinit var afp: AFPOffentligLivsvarigSimuleringService
+    private val log = KotlinLogging.logger {}
     fun grupperMedDatoFra(utbetalingsliste: List<Utbetalingsperiode>, foedselsdato: LocalDate): List<Maanedsutbetaling> {
         return utbetalingsliste
             .groupBy { it.fom }
@@ -14,5 +21,18 @@ object TpUtil {
                 Maanedsutbetaling(datoFra, bestemUttaksalderVedDato(foedselsdato, datoFra), totalMaanedsBelop)
             }
             .sortedBy { it.fraOgMedDato }
+    }
+
+    fun sammenlignOgLoggAfp(request: SimulerTjenestepensjonRequestDto, utbetalingsperiode: List<Utbetalingsperiode>) {
+        val afpLokal = afp.simuler(
+            SimulerAFPOffentligLivsvarigRequest(
+            fnr = request.pid,
+            fom = request.uttaksdato,
+            fodselsdato = request.foedselsdato,
+            fremtidigeInntekter = request.fremtidigeInntekter?.map { FremtidigInntekt(it.aarligInntekt,it.fraOgMed) } ?: emptyList()
+            )
+        )
+        val afpFraTpOrdning = utbetalingsperiode.filter { it.ytelseType == "OAFP" }
+        log.info { "AFP fra Tp ordning: $afpFraTpOrdning \n AFP fra lokal $afpLokal" }
     }
 }
