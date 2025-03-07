eee0adb82974a79607b9c6043b8fd84ebee871a4
martinstorvoll <martin.storvoll@nav.no>
2025-03-07 10:46:18 +0100

    Bruker SPKMapper for å kalkulere fremtidig inntekt. Burde refaktoreres

--- src/main/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/SammenlignAFPService.kt
+++ src/main/kotlin/no/nav/tjenestepensjon/simulering/v2025/tjenestepensjon/v1/service/SammenlignAFPService.kt
@@ -6,35 +6,25 @@ import no.nav.tjenestepensjon.simulering.model.domain.pen.SimulerAFPOffentligLiv
 import no.nav.tjenestepensjon.simulering.v2025.afp.v1.AFPOffentligLivsvarigSimuleringService
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.domain.Utbetalingsperiode
 import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.dto.request.SimulerTjenestepensjonRequestDto
+import no.nav.tjenestepensjon.simulering.v2025.tjenestepensjon.v1.service.spk.SPKMapper
 import org.springframework.stereotype.Service
-import java.time.LocalDate
 
 @Service
 class SammenlignAFPService(private val afp: AFPOffentligLivsvarigSimuleringService) {
     private val log = KotlinLogging.logger {}
     fun sammenlignOgLoggAfp(request: SimulerTjenestepensjonRequestDto, utbetalingsperiode: List<Utbetalingsperiode>) {
+        val fremtidigInntekt = SPKMapper.mapToRequest(request).fremtidigInntektListe
+
         val afpLokal = afp.simuler(
             SimulerAFPOffentligLivsvarigRequest(
                 fnr = request.pid,
                 fom = request.uttaksdato,
                 fodselsdato = request.foedselsdato,
-                fremtidigeInntekter = listOf(
-                    opprettNaaverendeInntektFoerUttak(request),
-                    FremtidigInntekt(
-                        0,
-                        request.uttaksdato
-                    )
-                ),
+                fremtidigeInntekter = fremtidigInntekt.map { FremtidigInntekt(it.aarligInntekt, it.fraOgMedDato ) }
             )
         )
         val afpFraTpOrdning = utbetalingsperiode.filter { it.ytelseType == "OAFP" }
         log.info { "AFP fra Tp ordning: $afpFraTpOrdning \n AFP fra lokal $afpLokal" }
     }
 
-    private fun opprettNaaverendeInntektFoerUttak(request: SimulerTjenestepensjonRequestDto) = FremtidigInntekt(
-        request.sisteInntekt,
-        fjorAarSomManglerOpptjeningIPopp()
-    )
-
-    private fun fjorAarSomManglerOpptjeningIPopp(): LocalDate = LocalDate.now().minusYears(1).withDayOfYear(1)
 }
